08c1acfcc3aa626cafc0a4ef9afdc1d52d52a9ce
Richard Hull <rm_hull@yahoo.co.uk>
2014-10-05 22:29:28 +0100

    Retab; print - if data_length = 0, putchar until NULL

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #if defined(__is_hulix_kernel)
@@ -9,65 +10,88 @@
 
 static void print(const char* data, size_t data_length)
 {
-	for ( size_t i = 0; i < data_length; i++ )
-		putchar((int) ((const unsigned char*) data)[i]);
+    if ( data_length == 0 )
+    {
+        unsigned char c;
+        while ((c = *data++) != '\0' )
+            putchar(c);
+    }
+    else
+    {
+        for ( size_t i = 0; i < data_length; i++ )
+            putchar((int) ((const unsigned char*) data)[i]);
+    }
 }
 
 int printf(const char* restrict format, ...)
 {
-	va_list parameters;
-	va_start(parameters, format);
+    va_list parameters;
+    va_start(parameters, format);
 
-	int written = 0;
-	size_t amount;
-	bool rejected_bad_specifier = false;
+    int written = 0;
+    size_t amount;
+    bool rejected_bad_specifier = false;
 
-	while ( *format != '\0' )
-	{
-		if ( *format != '%' )
-		{
-		print_c:
-			amount = 1;
-			while ( format[amount] && format[amount] != '%' )
-				amount++;
-			print(format, amount);
-			format += amount;
-			written += amount;
-			continue;
-		}
+    while ( *format != '\0' )
+    {
+        if ( *format != '%' )
+        {
+        print_c:
+            amount = 1;
+            while ( format[amount] && format[amount] != '%' )
+                amount++;
+            print(format, amount);
+            format += amount;
+            written += amount;
+            continue;
+        }
 
-		const char* format_begun_at = format;
+        const char* format_begun_at = format;
 
-		if ( *(++format) == '%' )
-			goto print_c;
+        if ( *(++format) == '%' )
+            goto print_c;
 
-		if ( rejected_bad_specifier )
-		{
-		incomprehensible_conversion:
-			rejected_bad_specifier = true;
-			format = format_begun_at;
-			goto print_c;
-		}
+        if ( rejected_bad_specifier )
+        {
+        incomprehensible_conversion:
+            rejected_bad_specifier = true;
+            format = format_begun_at;
+            goto print_c;
+        }
 
-		if ( *format == 'c' )
-		{
-			format++;
-			char c = (char) va_arg(parameters, int /* char promotes to int */);
-			print(&c, sizeof(c));
-		}
-		else if ( *format == 's' )
-		{
-			format++;
-			const char* s = va_arg(parameters, const char*);
-			print(s, strlen(s));
-		}
-		else
-		{
-			goto incomprehensible_conversion;
-		}
-	}
+        if ( *format == 'c' )
+        {
+            format++;
+            char c = (char) va_arg(parameters, int /* char promotes to int */);
+            print(&c, sizeof(c));
+        }
+        else if ( *format == 's' )
+        {
+            format++;
+            const char* s = va_arg(parameters, const char*);
+            print(s, 0);
+        }
+        else if ( *format == 'd' )
+        {
+            format++;
+            const int i = va_arg(parameters, int);
+            const char *s = itoa(i, NULL, 10);
+            print(s, 0);
+        }
+        else if ( *format == 'x' )
+        {
+            format++;
+            const int i = va_arg(parameters, int);
+            const char *s = itoa(i, NULL, 16);
+            print(s, 0);
+        }
+        else
+        {
+            goto incomprehensible_conversion;
+        }
+    }
 
-	va_end(parameters);
+    va_end(parameters);
 
-	return written;
+    return written;
 }
