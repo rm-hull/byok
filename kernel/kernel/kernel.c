76e4bdd691b05640d910714768f713aa88d74131
Richard Hull <rm_hull@yahoo.co.uk>
2014-10-05 22:30:07 +0100

    Scrolling check

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,15 +2,21 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
- 
+
 #include <kernel/tty.h>
- 
+
 void kernel_early(void)
 {
-	terminal_initialize();
+    terminal_initialize();
 }
- 
+
 void kernel_main(void)
 {
-	printf("Hello, kernel World!\n");
+    printf("===============================================================================\n");
+    printf("Hulix: Forth interpreter\n");
+    printf("\n\n\n\n\n\n\nHello World");
+
+    for (int i = 0; i < 3000; i++) {
+        printf("Line %d: %x\n", i, 3000 - i);
+    }
 }
