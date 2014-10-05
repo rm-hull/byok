7690bebbbd1460c65e8910941f30e0560072e52b
Richard Hull <rm_hull@yahoo.co.uk>
2014-10-05 22:25:03 +0100

    externs

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -1,18 +1,18 @@
 #ifndef _STDIO_H
 #define _STDIO_H 1
- 
+
 #include <sys/cdefs.h>
- 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
- 
-int printf(const char* __restrict, ...);
-int putchar(int);
-int puts(const char*);
- 
+
+extern int printf(const char* __restrict, ...);
+extern int putchar(int);
+extern int puts(const char*);
+
 #ifdef __cplusplus
 }
 #endif
- 
+
 #endif
--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -1,17 +1,17 @@
 #ifndef _STDLIB_H
 #define _STDLIB_H 1
- 
+
 #include <sys/cdefs.h>
- 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
- 
+
 __attribute__((__noreturn__))
-void abort(void);
- 
+extern void abort(void);
+
 #ifdef __cplusplus
 }
 #endif
- 
+
 #endif
