325e50c3a3ad19c18dedc8e04eee3a0dda4e945b
Richard Hull <rm_hull@yahoo.co.uk>
2014-10-11 23:51:08 +0100

    Move I/O functions into inline asm

--- kernel/include/kernel/asm/io.h
+++ kernel/include/kernel/asm/io.h
@@ -1,18 +1,19 @@
-#include <stdlib.h>
-#include <kernel/system.h>
+#ifndef __ASM_IO_H
+#define __ASM_IO_H
 
 /* Read/write to I/O ports to read/send bytes to devices.
    Inline assembly for the stuff that cannot be done in C */
 
-unsigned char inportb (uint16_t port)
+static inline unsigned char inportb (uint16_t port)
 {
     unsigned char rv;
     __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (port));
     return rv;
 }
 
-void outportb (uint16_t port, unsigned char data)
+static inline void outportb (uint16_t port, unsigned char data)
 {
     __asm__ __volatile__ ("outb %1, %0" : : "dN" (port), "a" (data));
 }
 
+#endif
