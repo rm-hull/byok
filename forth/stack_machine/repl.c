cbfc8852a8ffec98c9243b11ee39e4ae3a3f5285
Richard Hull <rm_hull@yahoo.co.uk>
2014-10-18 19:37:54 +0100

    basic noddy repl

--- forth/stack_machine/repl.c
+++ forth/stack_machine/repl.c
@@ -2,30 +2,74 @@
 #include <kernel/tty.h>
 #include <kernel/system.h>     // TODO - move readline out of here
 
+#include <collections/stack.h>
+
 #define READLINE_BUFSIZ 256
+#define DELIMITERS " \t\n"
 
-void show_prompt(int stack_size)
+void show_prompt(int state, int stack_size)
 {
-    terminal_setcolor(0x0F);
-    terminal_writestring("  ok");
-    terminal_setcolor(0x07);
-    for (int i = 0; i < stack_size; i++)
-        terminal_putchar('.');
+    if (state)
+    {
+        terminal_writestring("|  ");
+    }
+    else
+    {
+        terminal_setcolor(0x0F);
+        terminal_writestring("  ok");
+        terminal_setcolor(0x07);
+        for (int i = 0; i < stack_size; i++)
+            terminal_putchar('.');
 
-    terminal_putchar('\n');
+        terminal_putchar('\n');
+    }
     terminal_flush();
+}
+
+int isnumber(char *s)
+{
+    // TODO: check cotains only digits
+    return strlen(s) > 0;
 }
 
 void repl()
 {
-    int stack_size = 0; // dummy
+    stack_t *ds = malloc(sizeof(stack_t));
+    stack_init(ds, free);
+
     char *input = (char *)malloc(READLINE_BUFSIZ);
 
     while (true)
     {
-        show_prompt(stack_size);
+        show_prompt(0, stack_size(ds));
         readline(input, READLINE_BUFSIZ);
+        char *token = strtok(input, DELIMITERS);
+        while (token != NULL)
+        {
+            char *s = trim(strdup(token));
 
-        stack_size++;
+            if (memcmp(s, ".", 2) == 0)
+            {
+                if (stack_empty(ds))
+                {
+                    char **data;
+                    stack_pop(ds, data);
+                    terminal_writestring(*data);
+                    terminal_putchar(' ');
+                    free(*data);
+                }
+                else
+                {
+                    printf("ERROR: stack underflow");
+                }
+            }
+            else if (isnumber(s))
+            {
+                char *word = strdup(s);
+                stack_push(ds, word);
+            }
+            token = strtok(NULL, DELIMITERS);
+            free(s);
+        }
     }
 }
