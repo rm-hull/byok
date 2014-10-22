acdc8c254e4153b453e1f7fbdbcd03b51367250e
Richard Hull <rm_hull@yahoo.co.uk>
2014-10-23 00:42:43 +0100

    More words

--- forth/src/words/io.c
+++ forth/src/words/io.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/system.h>
 
 #include <stack_machine/common.h>
 #include <stack_machine/context.h>
@@ -20,6 +21,21 @@ int __DOT(context_t *ctx)
         return stack_underflow();
     }
 }
+
+
+int __UDOT(context_t *ctx)
+{
+    int num;
+    if (popnum(ctx->ds, &num)) {
+        printf("%d ", (unsigned int)num);
+        return true;
+    }
+    else
+    {
+        return stack_underflow();
+    }
+}
+
 
 int __DOT_S(context_t *ctx)
 {
@@ -46,6 +62,12 @@ int __EMIT(context_t *ctx)
     {
         return stack_underflow();
     }
+}
+
+
+int __KEY(context_t *ctx)
+{
+    pushnum(ctx->ds, getchar());
 }
 
 
@@ -91,9 +113,11 @@ void init_io_words(hashtable_t *htbl)
     add_entry(htbl, ".",      __DOT,    "( n -- )", "convert signed number n to string of digits, and output.");
     add_entry(htbl, ".S",     __DOT_S,  "( -- )", "display stack contents.");
     add_entry(htbl, "EMIT",   __EMIT,   "( ascii -- )", "outputs ascii as character.");
+    add_entry(htbl, "KEY",    __KEY,    "( -- ascii )", "waits for key, returns ascii.");
     add_entry(htbl, "CR",     __CR,     "( -- )", "outputs a line break.");
     add_entry(htbl, "SPACE",  __SPACE,  "( -- )", "outputs one single space character.");
     add_entry(htbl, "SPACES", __SPACES, "( u -- )", "outputs u space characters.");
     add_entry(htbl, "PAGE",   __CLS,    "( -- )", "clear screen.");
     add_entry(htbl, "CLS",    __CLS,    "( -- )", "clear screen.");
+    add_entry(htbl, "U.",     __UDOT,   "( u -- )", "convert unsigned number n to string of digits, and output.");
 }
