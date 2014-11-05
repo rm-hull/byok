7703666c230dc64f58076d39f8594093a65cb96e
Richard Hull <rm_hull@yahoo.co.uk>
2014-11-05 01:20:48 +0000

    docgen - escapes pipes & sorts words in sections

--- docgen.py
+++ docgen.py
@@ -26,7 +26,8 @@ if __name__ == '__main__':
         print
         print "| Word | Stack Effect | Description |"
         print "|------|--------------|-------------|"
-        for fields in csv.reader(extract_entries(fname), skipinitialspace=True):
+        records = csv.reader(extract_entries(fname), skipinitialspace=True)
+        for fields in sorted(records, key=lambda fields: fields[1]):
             _, word, _, stack_effect, docstring = fields
 
-            print "| {0} | {1} | {2} |".format(word, stack_effect, docstring[:-2])
+            print "| {0} | {1} | {2} |".format(word, stack_effect.replace('|', '\\|'), docstring[:-2])
