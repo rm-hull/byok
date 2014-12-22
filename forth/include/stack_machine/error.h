4b1784eaaf522b98cd19a31039d7546941640cb0
Richard Hull <rm_hull@yahoo.co.uk>
2014-12-22 01:15:25 +0000

    Errors with custom messages

--- forth/include/stack_machine/error.h
+++ forth/include/stack_machine/error.h
@@ -4,6 +4,7 @@
 #include <stack_machine/context.h>
 
 extern state_t sm_abort(context_t *ctx);
+extern state_t error_msg(context_t *ctx, int errno, char *msg, ...);
 extern state_t error(context_t *ctx, int errno);
 extern state_t stack_underflow(context_t *ctx);
 
