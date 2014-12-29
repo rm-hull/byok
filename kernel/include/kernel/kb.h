0d82d8056f926d6a3ebef8e6b402a672f8eb2759
Richard Hull <rm_hull@yahoo.co.uk>
2014-12-29 15:53:15 +0000

    Move key constants into separate header

---
+++ kernel/include/kernel/kb.h
@@ -0,0 +1,31 @@
+#ifndef __KB_H
+#define __KB_H
+
+#define KEY_CTRL_A    0
+#define KEY_CTRL_E    4
+#define KEY_CTRL_H    7
+#define KEY_DELETE  -46
+#define KEY_INSERT  -48
+#define KEY_END     -49
+#define KEY_DOWN    -50
+#define KEY_PGDN    -51
+#define KEY_LEFT    -52
+#define KEY_RIGHT   -54
+#define KEY_HOME    -55
+#define KEY_UP      -56
+#define KEY_PGUP    -57
+#define KEY_BACKSPACE       '\b'
+#define KEY_NEW_LINE        '\n'
+#define KEY_CARRIAGE_RETURN '\r'
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- kernel/src/arch/i386/kb.c
+++ kernel/src/arch/i386/kb.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <kernel/system.h>
 #include <kernel/tty.h>
+#include <kernel/kb.h>
 
 #define QUEUE_SIZE 128
 
@@ -38,22 +39,6 @@ const char caps_map[] =
     '2', '3', '0', '.', 0, 0, 0, 0, 0
 };
 
-#define KEY_CTRL_A    0
-#define KEY_CTRL_E    4
-#define KEY_CTRL_H    7
-#define KEY_DELETE  -46
-#define KEY_INSERT  -48
-#define KEY_END     -49
-#define KEY_DOWN    -50
-#define KEY_PGDN    -51
-#define KEY_LEFT    -52
-#define KEY_RIGHT   -54
-#define KEY_HOME    -55
-#define KEY_UP      -56
-#define KEY_PGUP    -57
-#define KEY_BACKSPACE       '\b'
-#define KEY_NEW_LINE        '\n'
-#define KEY_CARRIAGE_RETURN '\r'
 
 typedef struct {
     char buffer[QUEUE_SIZE];
