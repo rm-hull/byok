db8c14cb117aa718ecbb9274722445bee0e01a69
Richard Hull <rm_hull@yahoo.co.uk>
2014-12-29 20:41:35 +0000

    keycodes for control characters

--- kernel/include/kernel/kb.h
+++ kernel/include/kernel/kb.h
@@ -2,8 +2,32 @@
 #define __KB_H
 
 #define KEY_CTRL_A    0
+#define KEY_CTRL_B    1
+#define KEY_CTRL_C    2
+#define KEY_CTRL_D    3
 #define KEY_CTRL_E    4
+#define KEY_CTRL_F    5
+#define KEY_CTRL_G    6
 #define KEY_CTRL_H    7
+#define KEY_CTRL_I    8
+#define KEY_CTRL_J    9
+#define KEY_CTRL_K   10
+#define KEY_CTRL_L   11
+#define KEY_CTRL_M   12
+#define KEY_CTRL_N   13
+#define KEY_CTRL_O   14
+#define KEY_CTRL_P   15
+#define KEY_CTRL_Q   16
+#define KEY_CTRL_R   17
+#define KEY_CTRL_S   18
+#define KEY_CTRL_T   19
+#define KEY_CTRL_U   20
+#define KEY_CTRL_V   21
+#define KEY_CTRL_W   22
+#define KEY_CTRL_X   23
+#define KEY_CTRL_Y   24
+#define KEY_CTRL_Z   25
+
 #define KEY_DELETE  -46
 #define KEY_INSERT  -48
 #define KEY_END     -49
@@ -14,6 +38,7 @@
 #define KEY_HOME    -55
 #define KEY_UP      -56
 #define KEY_PGUP    -57
+
 #define KEY_BACKSPACE       '\b'
 #define KEY_NEW_LINE        '\n'
 #define KEY_CARRIAGE_RETURN '\r'
