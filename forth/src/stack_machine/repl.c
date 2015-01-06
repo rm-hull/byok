3b2995b9e4e7475d64f8ff149cbc7c32df1fd3d4
Richard Hull <rm_hull@yahoo.co.uk>
2015-01-06 00:38:34 +0000

    Abstracted load into separate file; Removed duplicate interpret() method

--- forth/src/stack_machine/repl.c
+++ forth/src/stack_machine/repl.c
@@ -40,7 +40,30 @@ void prompt(context_t *ctx)
     terminal_flush();
 }
 
-state_t interpret(context_t *ctx, char *in);
+context_t *load(context_t *ctx, char *filename, char *buf)
+{
+    char *saveptr;
+    char *line = strtok_r(buf, "\n", &saveptr);
+    int lineno = 1;
+
+    while (line != NULL)
+    {
+        interpret(ctx, line);
+        if (ctx->state == ERROR)
+        {
+            printf("  in '%s' at line %d:\n", filename, lineno);
+            terminal_setcolor(0x0F);
+            printf("%s\n", line);
+            terminal_setcolor(0x07);
+            break;
+        }
+
+        line = strtok_r(NULL, "\n", &saveptr);
+        lineno++;
+    }
+
+    return ctx;
+}
 
 context_t *init_context()
 {
@@ -78,15 +101,7 @@ context_t *init_context()
     init_memory_words(ctx);
 
     // bootstrap forth system proper
-    char *buf = (char*) &system_forth;
-    char *saveptr;
-    char *line = strtok_r(buf, "\n", &saveptr);
-
-    while (line != NULL && ctx->state != ERROR)
-    {
-        interpret(ctx, line);
-        line = strtok_r(NULL, "\n", &saveptr);
-    }
+    load(ctx, "system.fth", &system_forth);
 
     return ctx;
 }
@@ -103,92 +118,6 @@ void mem_stress_test()
         i++;
     }
 }
-
-state_t interpret(context_t *ctx, char *in)
-{
-    int n = strlen(in);
-    if (n == 0)
-    {
-        return OK;
-    }
-
-    if (n >= READLINE_BUFSIZ)
-    {
-        return error_msg(ctx, -1, ": input larger than TIB size");
-    }
-
-    // Copy the input buffer into the context's TIB, then again for tokenizing
-    memcpy(ctx->tib->buffer, in, n + 1);
-    char *inbuf = strdup(ctx->tib->buffer);
-    assert(inbuf != NULL);
-
-    // Begin parsing tokens proper
-    ctx->tib->token = strtok_r(inbuf, DELIMITERS, ctx->tib->saveptr);
-    while (ctx->tib->token != NULL)
-    {
-        // Set the current offset
-        ctx->tib->cur_offset = ctx->tib->token - inbuf;
-        char *s = trim(strdup(ctx->tib->token));
-        assert(s != NULL);
-
-        // Is this a word already in the dictionary?
-        entry_t *entry;
-        if (find_entry(ctx->exe_tok, s, &entry) == 0)
-        {
-            // Word exists, so set the contents of the word register
-            // to the dictionary param,
-            ctx->w = entry->param;
-
-            if (immediate_mode(entry) || ctx->state != SMUDGE)
-            {
-                // Execute immediately if word is marked as IMMEDIATE,
-                // or not in compile mode
-                state_t retval = entry->code_ptr(ctx);
-
-                // Only propagte the state if an error has been signalled.
-                // Certainly don't set to OK if in smudge mode.
-                if (retval != OK || ctx->state != SMUDGE)
-                    ctx->state = retval;
-            }
-            else
-            {
-                // Otherwise compile the execution token into the currently defined word
-                compile(ctx, 1, (int)entry);
-            }
-        }
-        else
-        {
-            int num;
-            if (parsenum(s, &num, ctx->base))
-            {
-                if (ctx->state == SMUDGE)
-                {
-                    literal(ctx, num);
-                }
-                else if (pushnum(ctx->ds, num))
-                {
-                    ctx->state = OK;
-                }
-                else
-                {
-                    // ??stack overflow??
-                }
-            }
-            else if (*s != '\0')
-            {
-                ctx->state = error_msg(ctx, -13, ": '%s'", s); // word not found
-            }
-        }
-        ctx->tib->token = strtok_r(NULL, DELIMITERS, ctx->tib->saveptr);
-        free(s);
-
-        if (ctx->state == ERROR) break;
-    }
-    free(inbuf);
-
-    return ctx->state;
-}
-
 
 void repl()
 {
